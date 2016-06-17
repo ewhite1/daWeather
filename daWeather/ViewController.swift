3ed519719c77a0ccbd1cb7722b78027d2b509ca5
Edward White <edwardwhite@outlook.com>
2016-06-17 02:10:14 -0500

    Initial Commit

---
+++ daWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  daWeather
+//
+//  Created by Edward White on 6/17/16.
+//  Copyright © 2016 edwardWhite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
